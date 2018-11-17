33741fac2853625776c4bfd188aa3d21051e664b
DimChristodoulou <dchristodoulou.di@gmail.com>
2018-11-18 01:56:17 +0200

    0.6 fifo

--- src/leafNode.c
+++ src/leafNode.c
@@ -19,11 +19,13 @@ int main(int argc, char *argv[]){
     int endRead = atoi(argv[3]);
     char* fileName = (char*)malloc(100*sizeof(char));
     char* patternName = (char*)malloc(100*sizeof(char));
+    char* myfifo = (char*)malloc(25*sizeof(char));
     char* ptr;
     strcpy(fileName, argv[4]);
     strcpy(patternName, argv[5]);
-
-    printf("In leafNode with %d pid, %d height, %d startRead, %d endRead, %s fileName, %s patternName\n", getpid(), height, startRead, endRead, fileName, patternName);
+    strcpy(myfifo, argv[6]);
+    int fd;
+    printf("In leafNode with %d pid, %d height, %d startRead, %d endRead, %s fileName, %s patternName %s myfifo\n", getpid(), height, startRead, endRead, fileName, patternName, myfifo);
 
     MyRecord searchRec;
     //char *searchStr = (char*)malloc( (5*SIZEofBUFF + SizeofFLOAT + SizeofINT + SizeofLONG) * sizeof(char));
@@ -31,21 +33,29 @@ int main(int argc, char *argv[]){
     char *houseIdStr = (char*)malloc(20*sizeof(char));
     char *amountStr = (char*)malloc(20*sizeof(char));
 
+    fd = open(myfifo, O_WRONLY);
+
     FILE *fptr = fopen(fileName, "rb");
     fseek( fptr, startRead*sizeof(MyRecord), SEEK_SET );
     for(int i = startRead; i < endRead; i++){
-        fread(&searchRec, sizeof(MyRecord), 1, fptr);      	
+        fread(&searchRec, sizeof(MyRecord), 1, fptr);   
+        
         sprintf(custIdStr, "%ld", searchRec.custid);
         sprintf(houseIdStr, "%d", searchRec.HouseID);
         sprintf(amountStr, "%-9.2f", searchRec.amount);
+
         if( strstr(custIdStr, patternName)!=NULL || strstr(searchRec.LastName, patternName)!=NULL || 
         strstr(searchRec.FirstName, patternName)!=NULL || strstr(searchRec.Street, patternName)!=NULL ||
         strstr(houseIdStr, patternName)!=NULL || strstr(searchRec.City, patternName)!=NULL || strstr(searchRec.postcode, patternName)!=NULL || 
         strstr(amountStr, patternName)!=NULL){
 
             
-            printf("%ld %s %s  %s %d %s %s %-9.2f\n", searchRec.custid, searchRec.LastName, searchRec.FirstName, searchRec.Street, searchRec.HouseID, searchRec.City, searchRec.postcode, searchRec.amount);
+            write(fd, &searchRec, sizeof(MyRecord)+1);
+            //printf("%ld %s %s  %s %d %s %s %-9.2f\n", searchRec.custid, searchRec.LastName, searchRec.FirstName, searchRec.Street, searchRec.HouseID, searchRec.City, searchRec.postcode, searchRec.amount);
         }
     }
-
+    write(fd, "test", 6);
+    printf("hello\n");
+    close(fd);
+    printf("bye\n");
 }
--- src/splitter-merger.c
+++ src/splitter-merger.c
@@ -10,6 +10,7 @@
 #include <CUnit/Basic.h>
 
 #include "../inc/shared.h"
+#include "../inc/tree.h"
 
 int main(int argc, char const *argv[])
 {
@@ -24,9 +25,28 @@ int main(int argc, char const *argv[])
     strcpy(patternName, argv[5]);
 
     int midRead = (startRead + endRead)/2;
+    char *pidStr = (char*)malloc(20*sizeof(char));
+    int pid = getpid();
+    sprintf(pidStr,"%d",pid);
 
-    char **argumentArray1 = (char**)malloc(7*sizeof(char*));
-	for(int i = 0; i < 7; i++)
+    char *myfifo1 = (char*)malloc(50*sizeof(char));
+    strcat(myfifo1, "tmp/fifo1");
+    strcat(myfifo1, pidStr);
+    if(mkfifo(myfifo1, 0666) < 0 ){
+        perror("fifo failed!");
+    } 
+
+    char *myfifo2 = (char*)malloc(25*sizeof(char));
+    strcat(myfifo2, "tmp/fifo2");
+    strcat(myfifo2, pidStr);
+    if(mkfifo(myfifo2, 0666) < 0 ){
+        perror("fifo failed!");
+    } 
+    
+    int fd1, fd2;
+
+    char **argumentArray1 = (char**)malloc(8*sizeof(char*));
+	for(int i = 0; i < 8; i++)
 		argumentArray1[i] = (char*)malloc(100*sizeof(char));
 	
     argumentArray1[0] = "exe/splitter-merger";
@@ -35,10 +55,11 @@ int main(int argc, char const *argv[])
 	sprintf(argumentArray1[3], "%d", midRead);
 	argumentArray1[4] = fileName;
 	argumentArray1[5] = patternName;
-	argumentArray1[6] = NULL;
+    argumentArray1[6] = myfifo1;
+	argumentArray1[7] = NULL;
 
-    char **argumentArray2 = (char**)malloc(7*sizeof(char*));
-	for(int i = 0; i < 7; i++)
+    char **argumentArray2 = (char**)malloc(8*sizeof(char*));
+	for(int i = 0; i < 8; i++)
 		argumentArray2[i] = (char*)malloc(100*sizeof(char));
 	
     argumentArray2[0] = "exe/splitter-merger";
@@ -47,8 +68,9 @@ int main(int argc, char const *argv[])
 	sprintf(argumentArray2[3], "%d", endRead);
 	argumentArray2[4] = fileName;
 	argumentArray2[5] = patternName;
-	argumentArray2[6] = NULL;
-
+    argumentArray2[6] = myfifo2;
+	argumentArray2[7] = NULL;
+    
     if(height > 1){
         pid_t pid1;
         pid1 = fork();
@@ -71,7 +93,7 @@ int main(int argc, char const *argv[])
         }
     }
     else if(height == 1){
-        pid_t pid1;
+        pid_t pid1;        
         pid1 = fork();
         if (pid1 == 0)
             execvp("exe/leafNode", argumentArray1);
@@ -86,9 +108,25 @@ int main(int argc, char const *argv[])
                 perror("Error in forking");
             else{
                 //IN PARENT
-                wait(NULL);
+                //wait(NULL);
+                MyRecord rec2;
+                char temp2[50];
+                printf("before open2\n");
+                fd2 = open(myfifo2, O_RDONLY);
+                printf("before read2\n");
+                read(fd2, temp2, 50);
+                printf("%s\n",temp2);
+                //printf("%ld %s %s  %s %d %s %s %-9.2f\n", rec2.custid, rec2.LastName, rec2.FirstName, rec2.Street, rec2.HouseID, rec2.City, rec2.postcode, rec2.amount);
             }
-            wait(NULL);
+            //wait(NULL);
+            MyRecord rec1;
+            char temp1[50];
+            printf("before open1\n");
+            fd1 = open(myfifo1, O_RDONLY);
+            printf("before read1\n");
+            read(fd1, temp1, 50);
+            printf("%s\n",temp1);
+            //printf("%ld %s %s  %s %d %s %s %-9.2f\n", rec1.custid, rec1.LastName, rec1.FirstName, rec1.Street, rec1.HouseID, rec1.City, rec1.postcode, rec1.amount);
         }
     }
 
